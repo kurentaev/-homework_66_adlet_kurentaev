da57a1bf3ff7a3020b9c96c4a57a68b0100380a1
Adlet Kurentaev <kurentaev@gmail.com>
2022-11-07 12:03:13 +0600

    Добавлено решение первого задания.

---
+++ js/main.js
@@ -0,0 +1,26 @@
+// Task 1
+const containers = document.querySelectorAll('.container')[0]
+
+const first = document.getElementById('string-1')
+const second = document.getElementById('string-2')
+const third = document.getElementById('string-3')
+const fourth = document.getElementById('string-4')
+const fifth = document.getElementById('string-5')
+const sixth = document.getElementById('string-6')
+
+function firstTask() {
+    containers.append(third, fifth)
+    containers.append(second, sixth)
+    containers.append(fourth, first)
+}
+
+// Task 2
+
+function secondTask() {
+    let elements = document.querySelectorAll('.element')
+
+    console.log(elements[i])
+}
+
+
+
